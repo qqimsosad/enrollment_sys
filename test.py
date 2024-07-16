2a1484ef27df66e249dd73159e27b2c869f512c2
Chris <111753227@g.nccu.edu.tw>
2024-07-16 12:28:12 +0800

    revise_test

--- test.py
+++ test.py
@@ -24,6 +24,13 @@ def test_create_teacher():
     }
     response = requests.post("http://127.0.0.1:5000/teachers", json=new_teacher)
     assert response.status_code == 201
+    data = response.json()
+    
+    assert 'teacher' in data
+    teacher_data = data['teacher']
+    assert 'teacher_id' in teacher_data and isinstance(teacher_data['teacher_id'], int), 'teacher_id must be an int'
+    assert 'teacher_name' in teacher_data and isinstance(teacher_data['teacher_name'], str), 'teacher_name must be an str'
+    assert 'teacher_email' in teacher_data and isinstance(teacher_data['teacher_email'], str), 'teacher_email must be an str'
     
 def test_update_course():
     updated_course = {
@@ -38,3 +45,14 @@ def test_update_course():
     }
     response = requests.put("http://127.0.0.1:5000/courses/1", json=updated_course)
     assert response.status_code == 200
+    data = response.json()
+    assert 'course' in data
+    course_data = data['course']
+    assert 'courseId' in course_data and isinstance(course_data['courseId'], int), 'courseId must be an int'
+    assert 'course_title' in course_data and isinstance(course_data['course_title'], str), 'course_title must be an str'
+    assert 'location' in course_data and isinstance(course_data['location'], str), 'location must be an str'
+    assert 'start_time' in course_data and isinstance(course_data['start_time'], str), 'start_time must be an str'
+    assert 'end_time' in course_data and isinstance(course_data['end_time'], str), 'end_time must be an str'
+    assert 'syllabus' in course_data and isinstance(course_data['syllabus'], str), 'syllabus must be an str'
+    assert 'teacher_name' in course_data and isinstance(course_data['teacher_name'], str), 'teacher_name must be an str'
+    assert 'teacher_email' in course_data and isinstance(course_data['teacher_email'], str), 'teacher_email must be an str'
